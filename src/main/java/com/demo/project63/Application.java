6a2e09762c5103bfea516be29a14454a438778dd
Arjun Surendra <asurendra@vmware.com>
2019-05-15 15:37:54 +0530

    code

--- src/main/java/com/demo/project63/Application.java
+++ src/main/java/com/demo/project63/Application.java
@@ -1,5 +1,7 @@
 package com.demo.project63;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,9 +40,8 @@ public class Application implements ApplicationRunner {
         KieSession kieSession1 = kContainer.newKieSession();
         Product p1 = new Product();
         p1.setType("desktop");
-        Map<String,String> r1 = new HashMap<>();
-        r1.put("region1","D");
-        p1.setRegions(r1);
+        p1.setRegions(Collections.EMPTY_MAP);
+        p1.setManufacturers(Collections.emptyList());
         kieSession1.insert(p1);
         kieSession1.fireAllRules();
         kieSession1.dispose();
@@ -55,11 +56,22 @@ public class Application implements ApplicationRunner {
         r2.put("region2","B");
         r2.put("region3","C");
         p2.setRegions(r2);
+        p2.setManufacturers(Collections.emptyList());
         kieSession2.insert(p2);
         kieSession2.fireAllRules();
         kieSession2.dispose();
         log.info("Discount on {} is {}", p2.getType(), p2.getDiscount());
 
+        //Iterates List
+        KieSession kieSession3 = kContainer.newKieSession();
+        Product p3 = new Product();
+        p3.setType("keyboard");
+        p3.setRegions(Collections.EMPTY_MAP);
+        p3.setManufacturers(Arrays.asList("Company1", "Company2"));
+        kieSession3.insert(p3);
+        kieSession3.fireAllRules();
+        kieSession3.dispose();
+        log.info("Discount on {} is {}", p3.getType(), p3.getDiscount());
 
     }
 
--- src/main/java/com/demo/project63/Product.java
+++ src/main/java/com/demo/project63/Product.java
@@ -1,5 +1,6 @@
 package com.demo.project63;
 
+import java.util.List;
 import java.util.Map;
 
 import lombok.Data;
@@ -11,4 +12,5 @@ public class Product {
     private String type;
     private int discount;
     private Map<String, String> regions;
+    private List<String> manufacturers;
 }
